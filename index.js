dd1dace9c045643a9663c237b8b3380fdb0c9465
Samuelfa <samuelfaalmeida2008@gmail.com>
2025-04-05 18:30:19 -0300

    finishing js

--- index.js
+++ index.js
@@ -11,13 +11,29 @@ function jogar() {
         EscolhaPersonagens = prompt("Quais vão ser seu companheiro"+ (i+1))
         Personagens[i] = EscolhaPersonagens; 
         //calcular a forca 
+        ForcaPersonagens += Math.floor(Math.random()*10 )+1 
+        console.log(ForcaPersonagens)
 
 }
-    for(let i=0; i < 3; i++){ 
+    for(let i=0; i < 3; i++){ //viloes
 
         IndiceAleatorio = Math.floor(Math.random()*5)
         PossiveisViloes = ['Vader','Darth Mau','Conde Ducan','Palpatine',"Troper"]
         Viloes[i] = PossiveisViloes[IndiceAleatorio] 
         console.log(Viloes)
+        ForcaViloes += Math.floor(Math.random()*10 )+1  
+        console.log(ForcaViloes)
+
+    }
+    if (ForcaPersonagens > ForcaViloes){ 
+        alert("Boa você escolheu bem, seu time ganhou a batalha contra: " + Viloes)
+    }
+    else { 
+        if( ForcaViloes > ForcaPersonagens){ 
+            alert("poxa parece que seu time perdeu a batalha contra:" + Viloes)
+        }
+        else{ 
+            alert("uau seu time lutou bravamente derrotou seus inimigos: "+ Viloes + " porem tambem caiu em batalha")
+        }
     }
 }
